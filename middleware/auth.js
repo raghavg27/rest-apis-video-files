aff70dba83301a69545e5238d765efbb641881b0
raghavg27 <raghavg2705@gmail.com>
2024-12-01 14:30:36 +0530

    static authentication

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,23 +1,15 @@
 // All API calls must be authenticated (static API tokens)
 
-const jwt = require("jsonwebtoken");
-const SECRET = process.env.SECRET;
+require("dotenv").config();
+
+const VALID_TOKENS = process.env.VALID_TOKENS ? process.env.VALID_TOKENS.split(",") : [];
 
 const authentication = (req, res, next) => {
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
-
-    if (!token) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
-
-    try {
-        const payload = jwt.decode(token, SECRET);
-        req.user = payload;
-        next();
-    } catch (error) {
-        return res.status(401).json({ message: 'Invalid token' });
-    }
+  const token = req.headers["authorization"];
+  if (!token || !VALID_TOKENS.includes(token)) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  next();
 };
 
 module.exports = authentication;
